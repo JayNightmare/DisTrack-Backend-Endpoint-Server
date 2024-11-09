0a2cf2e6e0e98ca0c0bb35e11294a625ff4d61f7
Jay <jordanbell321@gmail.com>
2024-11-09 20:00:24 +0000

    linode server commit

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const app = express();
-require('dotenv').config();
-
+const { connectToDatabase } = require('./database.js');
 const PORT = process.env.PORT || 3000;
+const User = require('./User.js');
 
 app.use(express.json());
+
+connectToDatabase();
 
 app.post('/coding-session', (req, res) => {
     const { user, duration } = req.body;
     console.log(`User: ${user}, Duration: ${duration} seconds`);
-
-    // Here, you would send this data to your Discord bot.
-    // For now, let's just respond with a success message.
     res.json({ message: 'Coding session recorded!' });
 });
 
+app.post('/link', async (req, res) => {
+    const { userId } = req.body;
+
+    try {
+        let user = await User.findOne({ userId });
+        if (!user) {
+            user = new User({ userId });
+            await user.save();
+        }
+
+        res.status(200).json({ message: "User linked successfully" });
+        console.log(`User ${userId} linked successfully.`);
+    } catch (error) {
+        console.error("Error linking user:", error);
+        res.status(500).json({ message: "Error linking user" });
+    }
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
