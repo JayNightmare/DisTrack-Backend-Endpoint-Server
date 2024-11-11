220a68d532ad24e6c7d1169a50f09a832823ac77
Jay <jordanbell321@gmail.com>
2024-11-11 18:36:03 +0000

    "Refactored database connection to use mongoose, removed GitHub webhook function and route, and updated server.js to remove related code"

--- config.js
+++ config.js
@@ -3,4 +3,5 @@ require("dotenv").config();
 module.exports = {
     MONGODB_URI: process.env.MONGODB_URI,
     PORT: process.env.PORT || 3000,
+    API_KEY: process.env.API_KEY
 };
--- database.js
+++ database.js
@@ -1,42 +1,18 @@
-// const mongoose = require("mongoose");
-// const { MONGODB_URI } = require("./config.js");
-const { MongoClient } = require('mongodb');
-
-// async function connectToDatabase() {
-//     try {
-//         await mongoose.connect(MONGODB_URI, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//             serverSelectionTimeoutMS: 20000,
-//             bufferCommands: false,
-//         });
-//         console.log("Connected to MongoDB");
-//     } catch (error) {
-//         console.error("Failed to connect to MongoDB:", error);
-//     }
-// }
-
-
-const uri = process.env.MONGODB_URI; // Use the MongoDB URI from the environment variable
-const dbName = process.env.DB; // Database name from environment variable
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    tls: true,
-    tlsAllowInvalidCertificates: true,
-    connectTimeoutMS: 30000,
-    socketTimeoutMS: 45000
-});
+const mongoose = require("mongoose");
+const { MONGODB_URI } = require("./config.js");
 
 async function connectToDatabase() {
-    if (!client.topology?.isConnected()) {
-        try {
-            await client.connect();
-            console.log("Connected to MongoDB");
-        } catch (error) {
-            console.error("Failed to connect to MongoDB:", error);
-        }
+    try {
+        await mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 20000,
+            bufferCommands: false,
+        });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
     }
-    return client.db(dbName);
 }
+
 module.exports = { connectToDatabase };
