c56b575e86e7106886b51eb6b61da03724530feb
Jay <jordanbell321@gmail.com>
2024-11-11 17:53:53 +0000

    "Switched from Mongoose to MongoDB driver, updated package dependencies"

--- database.js
+++ database.js
@@ -1,18 +1,31 @@
-const mongoose = require("mongoose");
-const { MONGODB_URI } = require("./config.js");
+// const mongoose = require("mongoose");
+// const { MONGODB_URI } = require("./config.js");
+const { MongoClient } = require('mongodb');
+
+// async function connectToDatabase() {
+//     try {
+//         await mongoose.connect(MONGODB_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             serverSelectionTimeoutMS: 20000,
+//             bufferCommands: false,
+//         });
+//         console.log("Connected to MongoDB");
+//     } catch (error) {
+//         console.error("Failed to connect to MongoDB:", error);
+//     }
+// }
+
+
+const uri = process.env.MONGODB_URI; // Use the MongoDB URI from the environment variable
+const db = process.env.DB;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connectToDatabase() {
-    try {
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 20000,
-            bufferCommands: false,
-        });
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Failed to connect to MongoDB:", error);
+    if (!client.isConnected()) {
+        await client.connect();
     }
+    return client.db(db);
 }
 
 module.exports = { connectToDatabase };
