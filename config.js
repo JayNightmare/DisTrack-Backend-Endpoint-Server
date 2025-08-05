f431815de81f13c78a12006126984f7cfd88f21b
Jay <jordanbell321@gmail.com>
2025-08-05 02:02:27 +0100

    Add Discord OAuth configuration and update server to handle authentication

--- config.js
+++ config.js
@@ -3,5 +3,7 @@ require("dotenv").config();
 module.exports = {
     MONGODB_URI: process.env.MONGODB_URI,
     PORT: process.env.PORT || 3000,
-    API_KEY: process.env.API_KEY
+    API_KEY: process.env.API_KEY,
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
 };
